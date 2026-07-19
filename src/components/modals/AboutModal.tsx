5dac648417d66a5950d0334d27fa23a25a638108
Navneet Aiyer <navneet.aiyer09@gmail.com>
2026-07-19 08:22:44 -0400

    vrindavan CYSP 2022 : Made changes to support Gita words. Made changes to support Gita version of the wordle. Update App.tsx, glosses.ts, index.html, AboutModal.tsx. Title Change.

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,19 +9,24 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500">
-        This is an{' '}
+        Thank You. This is an{' '}
         <a
           href="https://github.com/skalyan91/wordle-sanskrit"
           className="underline font-bold"
         >
           open source
-        </a>{' '}
-        clone of{' '}
+        </a> adapted from{' '}
+        <a
+          href="https://github.com/skalyan91/wordle-sanskrit"
+          className="underline font-bold"
+        >
+          wordle-sanskrit
+          </a> origninally taken from  
         <a
           href="https://www.powerlanguage.co.uk/wordle/"
           className="underline font-bold"
         >
-          the popular, original game by Josh Wardle
+           the popular, original game by Josh Wardle
         </a>{' '}
         based on the Sanskrit Dictionary at the{' '}
         <a href="http://www.sanskrit-linguistics.org/dcs/" className="underline font-bold">
